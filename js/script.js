4f589d6e3ca6d28ed9b267edd7dc8a87ce114126
Matheus Franco <sousafrancomatheus@gmail.com>
2022-07-04 13:59:27 -0300

    adicionando ofertas especiais e chamada para valores

--- js/script.js
+++ js/script.js
@@ -8,3 +8,19 @@ new SimpleSlide({
 if (window.SimpleAnime) {
   new SimpleAnime();
 }
+
+/* $(".show-more a").on("click", function() {
+  var $this = $(this); 
+  var $content = $this.parent().prev("div.content");
+  var linkText = $this.text().toUpperCase();    
+
+  if(linkText === "SHOW MORE"){
+      linkText = "Show less";
+      $content.switchClass("hideContent", "showContent", 400);
+  } else {
+      linkText = "Show more";
+      $content.switchClass("showContent", "hideContent", 400);
+  };
+
+  $this.text(linkText);
+});​ */
